1fb49dec5397de9fc5d61e6f7088150c9c499718
beenyamin <beenyaminkhan@gmail.com>
2023-12-12 23:08:10 +0600

    added halmet

--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -1,7 +1,11 @@
+import { Helmet } from "react-helmet-async";
 
 const Contact = () => {
     return (
         <div className="h-screen pt-20">
+            <Helmet>
+                <title> Yamin | Contact </title>
+            </Helmet>
             Contact Page
         </div>
     );
--- src/Pages/Project/Project.jsx
+++ src/Pages/Project/Project.jsx
@@ -1,8 +1,12 @@
+import { Helmet } from "react-helmet-async";
 
 
 const Project = () => {
     return (
         <div className="h-screen pt-20">
+            <Helmet>
+                <title> Yamin | Projects </title>
+            </Helmet>
             Project page
         </div>
     );
